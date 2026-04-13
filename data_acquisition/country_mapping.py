8004dacee92a5d6aa4c2a3ce5ac370b62a7680ce
ThivejaThirukumar <thiveja.thirukumar@gmail.com>
2026-04-13 19:43:31 +0200

    Update country-code-mapping
    Syntax Error removed

--- data_acquisition/country_mapping.py
+++ data_acquisition/country_mapping.py
@@ -1,6 +1,10 @@
 import csv 
+from pathlib import Path
 
-with open('../data/precious_metals_trade_inflation.csv') as f:
+BASE_DIR = Path(__file__).parent.parent  
+CSV_PATH = BASE_DIR / "data" / "processed" / "precious_metals_trade_inflation.csv"
+
+with open(CSV_PATH) as f:
     reader = csv.DictReader(f)
     laender = []
     for row in reader:
