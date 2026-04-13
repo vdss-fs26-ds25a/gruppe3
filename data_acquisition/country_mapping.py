b5a9ad60d5244d9bf3fa76e0fff887d0828252d8
ThivejaThirukumar <thiveja.thirukumar@gmail.com>
2026-04-13 20:45:54 +0200

    Update country_mapping.py

--- data_acquisition/country_mapping.py
+++ data_acquisition/country_mapping.py
@@ -1,6 +1,7 @@
 import csv 
 from pathlib import Path
-import pycountry 
+import pycountry
+
 
 BASE_DIR = Path(__file__).parent.parent  
 CSV_PATH = BASE_DIR / "data" / "processed" / "precious_metals_trade_inflation.csv"
@@ -16,9 +17,55 @@ print(len(laender)) #check
 gefunden = {}
 nicht_gefunden = []
 
+korrekturen = {
+    "Korea, Rep.": "KR",
+    "Turkey": "TR",
+    "Russia": "RU",
+    "USA": "US",
+    "Vietnam": "VN",
+    "Iran": "IR",
+    "Tanzania": "TZ",
+    "Bolivia": "BO",
+    "Venezuela, RB": "VE",
+    "Egypt, Arab Rep.": "EG",
+    "Kyrgyz Republic": "KG",
+    "Lao PDR": "LA",
+    "Gambia, The": "GM",
+    "Yemen, Rep.": "YE",
+    "Slovak Republic": "SK",
+    "Czech Rep.": "CZ",
+    "Central African Rep.": "CF",
+    "Cote d'Ivoire": "CI",
+    "Cape Verde": "CV",
+    "Swaziland": "SZ",
+    "Macao SAR, China": "MO",
+    "Hong Kong SAR, China": "HK",
+    "Macedonia, FYR": "MK",
+    "Bosnia Herzegovina": "BA",
+    "St. Lucia": "LC",
+    "St. Kitts and Nevis": "KN",
+    "St. Vincent and the Grenadines": "VC",
+    "Solomon Isds": "SB",
+    "Cook Isds": "CK",
+    "Faeroe Isds": "FO",
+    "Turks and Caicos Isds": "TC",
+    "Micronesia, Fed. Sts.": "FM",
+    "Neth. Antilles": "AN",
+}
+
 for name in laender:
     ergebnis = pycountry.countries.get(name=name)
     if ergebnis:
         gefunden[name] = ergebnis.alpha_2
     else:
-        nicht_gefunden.append(name)
+        try: 
+            ergebnis = pycountry.countries.search_fuzzy(name)
+            gefunden[name] = ergebnis[0].alpha_2
+        except LookupError:
+            if name in korrekturen:
+                gefunden[name] = korrekturen[name]
+            else:
+                nicht_gefunden.append(name)
+
+print("O Gefunden:", len(gefunden))
+print("X Nicht gefunden:", nicht_gefunden)
